4fecbe6868cea5f395044f042624ee53093681cc
tuan2004a <bi03112004@gmail.com>
2025-03-06 15:40:05 +0700

    style: introduce

--- src/component/Introduce.jsx
+++ src/component/Introduce.jsx
@@ -4,10 +4,10 @@ const Introduce = () => {
         <div className="max-w-[750px] mx-auto text-white py-20 px-5 lg:max-w-[1300px] sm:px-10 xl:px-0">
             <div className="w-full flex flex-wrap items-center justify-between md:flex-nowrap">
                 <div className="flex flex-col items-center w-full md:w-1/2">
-                    <h1 className="text-left font-bold w-full text-2xl lg:pl-20 md:text-xl lg:text-3xl">
-                        Xin chào, Tôi là Lâm Tuấn <br /> <span className="inline-block font-bold text-transparent bg-linear-to-r from-purple-600 to-white bg-clip-text leading-none mt-2.5 text-5xl md:text-4xl lg:text-6xl">Web Developer</span>
+                    <h1 className="text-left font-bold w-full text-xl lg:pl-20 md:text-xl lg:text-3xl">
+                        Xin chào, Tôi là Lâm Tuấn <br /> <span className="inline-block font-bold text-transparent bg-linear-to-r from-purple-600 to-white bg-clip-text leading-none mt-2.5 text-3xl md:text-4xl lg:text-6xl">Web Developer</span>
                     </h1>
-                    <div className="size-[430px] overflow-hidden relative rounded-4xl border-2 border-[#8750f7]/50 cursor-default transition-all duration-400 rotate-6 hover:rotate-0 hover:border-[#8750f7] my-10 md:hidden">
+                    <div className="size-[320px] sm:size-[380px] md:size-[430px] overflow-hidden relative rounded-4xl border-2 border-[#8750f7]/50 cursor-default transition-all duration-400 rotate-6 hover:rotate-0 hover:border-[#8750f7] my-10 md:hidden">
                         <img src="/image/avat2.jpg" alt="" className="overflow-hidden " />
                     </div>
                     <p className="text-xl font-light leading-relaxed mt-5 text-zinc-300 w-full lg:pl-20 md:max-lg:text-lg">Sinh viên năm cuối ngành công nghệ thông tin đang tìm kiếm cơ hội  phát triển bản thân với vai  trò intern developer</p>
@@ -33,7 +33,7 @@ const Introduce = () => {
                         </div>
                     </div>
                 </div>
-                <div className="w-1/2 hidden flex items-center justify-center px-5 lg:px-20 md:block">
+                <div className="w-1/2 hidden px-5 lg:px-20 md:block">
                     <div className="relative rounded-4xl overflow-hidden border-2 border-[#8750f7]/50 cursor-default transition-all duration-400 rotate-6 bg-cover bg-top bg-no-repeat bg-[url()] hover:rotate-0 hover:border-[#8750f7] md:size-[320px] lg:size-[380px] xl:size-[430px] ">
                         <img src="/image/avat2.jpg" alt="" />
                     </div>
